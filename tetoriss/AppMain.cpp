d966f0237efb826c75996fabc37f4c3c7b9cbca4
ttry <138739136+ttrmsy@users.noreply.github.com>
2023-10-06 12:12:42 +0900

    ヘッダーの追加
    ヘッダープログラムの追加

---
+++ tetoriss/AppMain.cpp
@@ -0,0 +1,7 @@
+/****************************************
+* プログラミング実習（テトリス編）
+*****************************************/
+#include "DxLib.h"
+#include"FreamControl.h"
+#include"SceneManager.h"
+#include"InputControl.h"
