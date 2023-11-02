f40c5660a35e169d71e92ba073c93cdae521e548
ttry <138739136+ttrmsy@users.noreply.github.com>
2023-11-02 15:21:45 +0900

    ranking.cpp
    プログラム追加

--- tetoriss/RankingScene.h
+++ tetoriss/RankingScene.h
@@ -3,7 +3,7 @@
 /*********************
 *マクロ定義
 **********************/
-#define RANKING_INPUT_MODE　(1)
+#define RANKING_INPUT_MODE (1)
 #define RANKING_DISP_MODE (2)
 
 /***********************
@@ -18,5 +18,5 @@ int RankingScene_Initialize(void);
 void RankingScene_Update(void);
 void RankingScene_Draw(void);
 
-void Set_RankigMode(int mode);
+void Set_RankingMode(int mode);
 void Set_RankingScore(int score);
